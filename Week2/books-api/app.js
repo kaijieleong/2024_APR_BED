d30ae71b4a770d4cc48ca568c077847dd71d4a5a
kaijieleong <s10262480@connect.np.edu.sg>
2024-04-22 11:20:35 +0800

    Part4 completed
    added the last app.delete

--- Week2/books-api/app.js
+++ Week2/books-api/app.js
@@ -33,3 +33,29 @@ app.get("/books/:id", (req, res) => {
     res.status(404).send("Book not found"); // Send error for non-existent book
   }
 });
+app.put("/books/:id", (req, res) => {
+  const bookId = parseInt(req.params.id); // Get book ID from URL parameter
+  const updatedBook = req.body; // Get updated book data from request body
+
+  const bookIndex = books.findIndex((book) => book.id === bookId);
+
+  if (bookIndex !== -1) {
+    updatedBook.id = bookId;
+    books[bookIndex] = updatedBook; // Update book data in the array
+    res.json(updatedBook); // Send updated book data
+  } else {
+    res.status(404).send("Book not found"); // Send error for non-existent book
+  }
+});
+app.delete("/books/:id", (req, res) => {
+  const bookId = parseInt(req.params.id); // Get book ID from URL parameter
+
+  const bookIndex = books.findIndex((book) => book.id === bookId);
+
+  if (bookIndex !== -1) {
+    books.splice(bookIndex, 1); // Remove book from the array
+    res.status(204).send(); // Send empty response with status code 204 (No Content)
+  } else {
+    res.status(404).send("Book not found"); // Send error for non-existent book
+  }
+});
