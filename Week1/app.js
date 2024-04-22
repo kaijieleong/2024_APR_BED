2bbb4c28ef8a33e321c3a97438d4fb50cd42c0aa
kaijieleong <s10262480@connect.np.edu.sg>
2024-04-22 10:47:34 +0800

    Added Node_Moudles to main folder
    remove Node_Modules from weekly folder

--- Week1/app.js
+++ Week1/app.js
@@ -25,3 +25,19 @@ fs.readFile("/notes.txt", "utf-8", (err, data) => {
   }
   console.log(data);
 });
+const chalk = require("chalk");
+console.log(chalk.yellow("hi"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+app.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+app.delete("/user", (req, res) => {
+  app.send("Got a DELETE request at /user");
+});
