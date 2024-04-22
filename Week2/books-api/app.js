2b07ab75655d103866dec5d2e0d14bce8b57ea8e
kaijieleong <s10262480@connect.np.edu.sg>
2024-04-22 11:13:05 +0800

    Part 2 Completed

--- Week2/books-api/app.js
+++ Week2/books-api/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 let books = [
@@ -8,9 +9,27 @@ let books = [
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}`);
 });
+
 app.use(express.json());
 // Configure body-parser to handle URL-encoded form data
-/*app.use(bodyParser.urlencoded({ extended: true })); // Set extended: true for nested objects
+app.use(bodyParser.urlencoded({ extended: true })); // Set extended: true for nested objects
 app.get("/books", (req, res) => {
   res.json(books); // Send the array of books as JSON response
-});*/
+});
+app.post("/books", (req, res) => {
+  const newBook = req.body; // Get the new book data from the request body
+  newBook.id = books.length + 1; // Assign a unique ID
+  books.push(newBook); // Add the new book to the array
+  res.status(201).json(newBook); // Send created book with status code 201
+});
+
+app.get("/books/:id", (req, res) => {
+  const bookId = parseInt(req.params.id); // Get book ID from URL parameter
+  const book = books.find((book) => book.id === bookId);
+
+  if (book) {
+    res.json(book); // Send the book data if found
+  } else {
+    res.status(404).send("Book not found"); // Send error for non-existent book
+  }
+});
